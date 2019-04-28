f13e3afee06e995d32c087f9f1f34c645c9c1de8
yichengjie <626659321@qq.com>
2019-04-28 10:20:20 +0800

    逆向工程测试

---
+++ src/main/java/com/yicj/mybatis/pojo/Dept.java
@@ -0,0 +1,23 @@
+package com.yicj.mybatis.pojo;
+
+public class Dept {
+    private Integer deptno;
+
+    private String deptname;
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDeptname() {
+        return deptname;
+    }
+
+    public void setDeptname(String deptname) {
+        this.deptname = deptname == null ? null : deptname.trim();
+    }
+}
